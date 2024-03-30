a9fa6ead819084c17dcdf53ffe13b1d46a79909e
Hair Zabala <hair.zabala@gmail.com>
2024-03-30 15:54:43 +1000

    feat: extra pages

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -1,5 +1,73 @@
+import PrimaryButton from "@/components/ui/ButtonComponent";
+import HeaderPage from "@/components/ui/HeaderPage";
+import QuantitySelector from "@/features/product/QuantitySelector";
+import { initialData } from "@/seed/seed";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
+const productsInCart = [
+  initialData.products[0],
+  initialData.products[1],
+  initialData.products[2],
+];
+
 export default function CartPage() {
-  return <div>CartPage</div>;
+  return (
+    <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
+      <div className="flex flex-col w-[1000px]">
+        <HeaderPage title="Cart" />
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
+          <div className="flex flex-col mt-5">
+            <span className="text-xl">Add more items</span>
+            <Link href="/" className="underline mb-5">
+              Keep shopping
+            </Link>
+            {productsInCart.map((product) => (
+              <div key={product.slug} className="flex mb-2">
+                <Image
+                  src={`/products/${product.images[0]}`}
+                  width={100}
+                  height={100}
+                  style={{
+                    width: "100px",
+                    height: "100px",
+                  }}
+                  alt={product.title}
+                  className="mr-5 rounded"
+                />
+                <div>
+                  <p>{product.title}</p>
+                  <p>{product.price}</p>
+                  <QuantitySelector quantity={1} />
+                  <button className="underline mt-3">Remove</button>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div className="bg-white rounded-xl shadow-xl p-7">
+            <h2 className="text-2xl mb-2">Order summary</h2>
+            <div className="grid grid-cols-2 ">
+              <span>No. Products</span>
+              <span className="text-right">3 items</span>
+
+              <span>Subtotal</span>
+              <span className="text-right">$100</span>
+
+              <span>Tax (15%)</span>
+              <span className="text-right">$100</span>
+
+              <span className="text-2xl mt-5">Total</span>
+              <span className="text-2xl mt-5 text-right">$100</span>
+            </div>
+
+            <Link href="/checkout/address">
+              <PrimaryButton text="Checkout" className="mt-5" />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/app/(shop)/checkout/address/page.tsx
+++ src/app/(shop)/checkout/address/page.tsx
@@ -1,5 +1,62 @@
-import React from "react";
+import DefaultButton from "@/components/ui/ButtonComponent";
+import HeaderPage from "@/components/ui/HeaderPage";
+import Link from "next/link";
 
 export default function AddressPage() {
-  return <div>AddressPage</div>;
+  return (
+    <div className="flex flex-col sm:justify-center sm:items-center mb-72 px-10 sm:px-0">
+      <div className="w-full  xl:w-[1000px] flex flex-col justify-center text-left">
+        <HeaderPage title="Address" subtitle="Delivery address" />
+
+        <div className="grid grid-cols-1 gap-2 sm:gap-5 sm:grid-cols-2">
+          <div className="flex flex-col mb-2">
+            <span>Names</span>
+            <input type="text" className="p-2 border rounded-md bg-gray-200" />
+          </div>
+
+          <div className="flex flex-col mb-2">
+            <span>Last name</span>
+            <input type="text" className="p-2 border rounded-md bg-gray-200" />
+          </div>
+
+          <div className="flex flex-col mb-2">
+            <span>Address</span>
+            <input type="text" className="p-2 border rounded-md bg-gray-200" />
+          </div>
+
+          <div className="flex flex-col mb-2">
+            <span>Address 2 (optional)</span>
+            <input type="text" className="p-2 border rounded-md bg-gray-200" />
+          </div>
+
+          <div className="flex flex-col mb-2">
+            <span>Postal code</span>
+            <input type="text" className="p-2 border rounded-md bg-gray-200" />
+          </div>
+
+          <div className="flex flex-col mb-2">
+            <span>City</span>
+            <input type="text" className="p-2 border rounded-md bg-gray-200" />
+          </div>
+
+          <div className="flex flex-col mb-2">
+            <span>Country</span>
+            <select className="p-2 border rounded-md bg-gray-200">
+              <option value="">[ select ]</option>
+              <option value="CRI">Costa Rica</option>
+            </select>
+          </div>
+
+          <div className="flex flex-col mb-2">
+            <span>Mobile number</span>
+            <input type="text" className="p-2 border rounded-md bg-gray-200" />
+          </div>
+
+          <Link href="/checkout" className="w-full sm:w-1/2 justify-center">
+            <DefaultButton text="Next" />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
